5c13fc0532ead3249256e2928e01ed06381a1925
vallemkr <vallemkr@umich.edu>
2020-07-26 17:53:19 -0400

    almost finished with mvp

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import { elements } from './elements.js';
 
 class elementContainer {
-    constructor(name, number, symbol) {
+    constructor(name, number, symbol, group) {
         this.name = name;
         this.number = number;
         this.symbol = symbol;
+        this.group = group.replace(/ /g, "-");
     }
 
     makeElementDiv() {
         const div = document.createElement("div");
         div.classList.add("elementDiv");
-        div.id = `element-${this.symbol}`;
+        div.classList.add(this.group);
+        div.id = this.symbol;
         div.textContent = this.symbol;
 
         const tableContainer = document.querySelector("#tableContainer");
@@ -19,6 +21,6 @@ class elementContainer {
 } 
 
 for (const element of elements) {
-    const container = new elementContainer(element.name, element.number, element.symbol);
+    const container = new elementContainer(element.name, element.number, element.symbol, element.category);
     container.makeElementDiv();
 }
