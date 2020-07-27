ef68a4db0beb1856107452ec94384cf84672c9bc
vallemkr <vallemkr@umich.edu>
2020-07-27 18:05:15 -0400

    added basic wikiBox functionallity

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import { elements } from './elements.js';
 
 class elementContainer {
-    constructor(name, number, symbol, group) {
+    constructor(name, number, symbol, category) {
         this.name = name;
         this.number = number;
         this.symbol = symbol;
         // regex removes spaces and replaces them with dashes to make valid css class
-        this.group = group.startsWith("unknown") ? "unknown" : group.replace(/ /g, "-");
+        this.category = category.startsWith("unknown") ? "unknown" : category.replace(/ /g, "-");
     }
 
     makeElementDiv() {
         const div = document.createElement("div");
         div.classList.add("elementDiv");
-        div.classList.add(this.group);
+        div.classList.add(this.category);
         div.id = this.symbol;
 
         const number = document.createElement("div");
@@ -31,12 +31,29 @@ class elementContainer {
         div.appendChild(symbol);
         div.appendChild(name);
 
+        div.onclick = toggleWikiBox;
+
         const tableContainer = document.querySelector("#tableContainer");
         tableContainer.appendChild(div);
     }
-} 
+}
+
+function toggleWikiBox() {
+    const wikiBoxVisible = document.querySelector("#wikiBox").style.display !== "none";
+
+    if (wikiBoxVisible){
+        document.querySelector("#tableContainer").style.display = "grid";
+        document.querySelector("#wikiBox").style.display = "none";  
+    }
+    else {
+        document.querySelector("#tableContainer").style.display = "none";
+        document.querySelector("#wikiBox").style.display = "block";  
+    }
+}
 
 for (const element of elements) {
     const container = new elementContainer(element.name, element.number, element.symbol, element.category);
     container.makeElementDiv();
 }
+
+document.querySelector("#closeBtn").onclick = toggleWikiBox;
